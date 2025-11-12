172c3e5cbb10c0b4647b7078c669fab227325ca2
Priyadarshini777 <mpriyadharu@gmail.com>
2025-11-12 22:43:47 +0530

    Rename VPC and subnet to avoid existing resource conflict

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,10 +1,10 @@
 resource "google_compute_network" "vpc" {
-  name = "custom-vpc"
+  name                    = "custom-vpc-2"  # 👈 Changed name to avoid conflict
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = "custom-vpc-subnet"
+  name          = "custom-vpc-subnet-2"     # 👈 Match new VPC name
   region        = var.region
   network       = google_compute_network.vpc.id
   ip_cidr_range = "10.0.0.0/16"
@@ -18,9 +18,9 @@ resource "google_artifact_registry_repository" "repo" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
-  network  = google_compute_network.vpc.id
+  name       = var.cluster_name
+  location   = var.region
+  network    = google_compute_network.vpc.id
   subnetwork = google_compute_subnetwork.subnet.id
 
   remove_default_node_pool = true
@@ -28,9 +28,9 @@ resource "google_container_cluster" "primary" {
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  name     = "primary-node-pool"
-  location = var.region
-  cluster  = google_container_cluster.primary.name
+  name       = "primary-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
   node_count = 2
 
   node_config {
